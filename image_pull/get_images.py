6c25c62f82e537eea4b632ad8d28005a834c7451
zankner <zackankner@gmail.com>
2018-11-11 12:27:25 -0500

    login for mymilton

---
+++ image_pull/get_images.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+import lxml.html
+
+s = requests.session()
+login = s.get('https://mymustangs.milton.edu/student/')
+login_html = lxml.html.fromstring(login.text)
+hidden_inputs = login_html.xpath("//form[@name='LoginForm']/@action")
+print(hidden_inputs)
+tokens = (hidden_inputs[0].split("?"))[1].split("&")
+cfid = tokens[0].split("=")
+cftok = tokens[1].split("=")
+print(cfid , cftok)
+cookies = {'CFID':cfid[1], 'CFTOKEN':cftok[1]}
+user = 'Zankner20'
+password = '4pcsuqi1'
+payload = {'UserLogin':user, 'UserPassword':password}
+response = s.post('https://mymustangs.milton.edu/student/', cookies = cookies, data=payload)
